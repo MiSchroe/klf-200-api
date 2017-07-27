cd6068368175bde9ed07d0c40fd21f7197409e7e
Michael Schroeder <Michael.Schroeder@pmone.com>
2017-07-27 15:16:19 +0200

    Added basic JSDoc documentation

--- src/products.js
+++ src/products.js
@@ -2,11 +2,19 @@
 
 const urlBuilder = require('./urlBuilder');
 
-
+/**
+ * Create a new products object.
+ * Use the products object to retrieve a list of products known to your KLF interface.
+ * @param {connection} connection The connection object that handles the communication to the KLF interface.
+ */
 function products(connection) {
     this.connection = connection;
 }
 
+/**
+ * Gets a list of your products.
+ * @return {Promise} Returns a promise that resolves to the products list.
+ */
 products.prototype.getAsync = function () {
     return this.connection.postAsync(urlBuilder.products, 'get', null)
         .then((res) => {
--- src/scenes.js
+++ src/scenes.js
@@ -2,11 +2,19 @@
 
 const urlBuilder = require('./urlBuilder');
 
-
+/**
+ * Create a new scenes object.
+ * Use the scenes object to retrieve a list of scenes known to your KLF interface and to start one of them.
+ * @param {connection} connection
+ */
 function scenes(connection) {
     this.connection = connection;
 }
 
+/**
+ * Get a list of the scenes stored in the KLF interface.
+ * @return {Promise} Returns a promise that resolves to the list of the scenes.
+ */
 scenes.prototype.getAsync = function () {
     return this.connection.postAsync(urlBuilder.scenes, 'get', null)
         .then((res) => {
@@ -14,6 +22,11 @@ scenes.prototype.getAsync = function () {
         });
 };
 
+/**
+ * Runs a scene either by ID or name.
+ * @param {(number|string)} sceneId The id or the name of the scene.
+ * @return {Promise} Returns a promise that resolves to the result of the API call.
+ */
 scenes.prototype.runAsync = function (sceneId) {
     if (!sceneId && sceneId !== 0)
         return Promise.reject(new Error('Missing sceneId parameter.'));
