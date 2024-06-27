5038784e2010df01235ca4f228f357c4258353ca
Michael Schröder <mschroeder@pmone.com>
2024-06-27 13:33:08 +0200

    Fix prepare-cjs script for older Node versions

--- deployment/prepare-cjs.ts
+++ deployment/prepare-cjs.ts
@@ -1,4 +1,5 @@
 import fs from "fs/promises";
+import path from "path";
 
 console.log("Removing directory ./src-cjs");
 await fs.rm("./src-cjs", { recursive: true, force: true });
@@ -7,7 +8,16 @@ console.log("Copying files from ./src to ./src-cjs");
 await fs.cp("./src", "./src-cjs", { recursive: true });
 
 console.log("Overwriting files with cjs versions");
-for await (const filename: string of fs.glob("./src-cjs/**/*.cjs.ts")) {
-	const newFileName = `${(<string>filename).slice(0, -7)}.ts`;
-	await fs.rename(filename, newFileName);
+async function* walk(dir: string): AsyncGenerator<string> {
+	for await (const d of await fs.opendir(dir)) {
+		const entry = path.join(dir, d.name);
+		if (d.isDirectory()) yield* walk(entry);
+		else if (d.isFile()) yield entry;
+	}
+}
+for await (const filename of walk("./src-cjs")) {
+	if (filename.endsWith(".cjs.ts")) {
+		const newFileName = `${(<string>filename).slice(0, -7)}.ts`;
+		await fs.rename(filename, newFileName);
+	}
 }
